a345655aefe8f452c3d8d0d9ce4f1703067220ed
madina987 <104410506+madina987@users.noreply.github.com>
2022-04-27 15:39:32 +0600

    Add files via upload

---
+++ script.py
@@ -0,0 +1,11 @@
+print('Hello World!')
+a = float(input("Введите коэффициент a "))
+b = float(input("Введите коэффициент b "))
+a = float(input("Введите коэффициент a "))
+b = float(input("Введите коэффициент b "))
+if (a == 0 and b == 0):
+    print("Бесконечное количество решений.")
+if (a == 0 and b != 0):
+    print("Решений нет.")
+if (a != 0):
+    print(b/a))
